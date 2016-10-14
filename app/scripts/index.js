5123847ee3f01b0c69093552924374216566677c
Colin McKeon <14mckeon@gmail.com>
2016-10-14 17:29:27 -0400

    bad guy displays

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,6 +1,7 @@
 // alert('Colin & Chase Rule All');
 var $ =  require('jquery');
 var models = require('./models');
+var stageTemplate = require('../templates/stage.hbs');
 var template = require('../templates/template.hbs');
 var dropdown = require('../templates/dropdown.hbs');
 var _ = require('underscore');
@@ -9,43 +10,57 @@ var _ = require('underscore');
 $(function(){
 
 var characterChoice;
+var selectedCharacter;
+var selectedBadGuy;
 
-var chars = [
+var goodGuys = [
   new models.GoodGuy({name: 'Neo', image:'http://static.comicvine.com/uploads/original/11119/111191852/5023934-4839031750-neo1.j.jpeg'}),
   new models.GoodGuy({name: 'Beatrix Kiddo', image: 'http://static1.comicvine.com/uploads/original/11119/111194436/5168520-7476152247-9ad58.jpg'}),
-  new models.GoodGuy({name: 'Bubbles', image: 'https://lh3.ggpht.com/QikRYDuc28DXrAv9-9i-W_4RTMdrlfO6SlfuZFXjnLuCd4AhDdaaDZJ5SmUT_6ibuWcE=w300'}),
+  new models.GoodGuy({name: 'Bubbles', image: 'https://lh3.ggpht.com/QikRYDuc28DXrAv9-9i-W_4RTMdrlfO6SlfuZFXjnLuCd4AhDdaaDZJ5SmUT_6ibuWcE=w300'})
+];
+
+var badGuys = [
   new models.BadGuy({name: 'Bill', image: 'https://s-media-cache-ak0.pinimg.com/originals/f4/da/6a/f4da6a305089e0c73a6f59b8070c5a53.jpg'}),
   new models.BadGuy({name: 'Agent Smith', image:'http://vignette1.wikia.nocookie.net/matrix/images/4/4d/Agent-smith-the-matrix-movie-hd-wallpaper-2880x1800-4710.png/revision/latest?cb=20140504013834'})
 ];
 
+
+
+
+
 // Dropdown of goodguys ***************************
 //*************************************************
-var goodGuys = _.filter(chars, {'color': 'green'});
 $('.ui').html(dropdown({'chars': goodGuys}));
 
-var $characterName;
 
 $(document).on('click', '.protagonist', function(event){
   event.preventDefault();
 
-  $characterName = $(this).text();
+  var characterName = $(this).text();
 
-  var selectedCharacter = goodGuys.filter(function(char){
-    return char.name === $characterName;
-  });
+  selectedCharacter = goodGuys.filter(function(char){
+    return char.name === characterName;
+  })[0];
+  var randomNum = _.random(0, badGuys.length-1);
+  selectedBadGuy = badGuys[randomNum];
 
-  console.log(selectedCharacter[0].color);
 
-  $('.ui').html(template(selectedCharacter[0]));
+
+  console.log(selectedCharacter.color);
+
+  $('.ui').html(stageTemplate());
+
+  $('.goodguy-holder').html(template(selectedCharacter));
+  $('.badguy-holder').html(template(selectedBadGuy));
 });
 
-// DISPLAYS BAD GUY *******************************
-//*************************************************
-
-// var badGuys = _.filter(char, {'color': 'red'});
 
 
+// Sets the src
+$('.attack-animation').find('img').show().attr('src', 'http://...');
 
+// Removes the src
+$('.attack-animation').find('img').hide().attr('src', '');
 
 
 // PUTS ARRAY INTO HTML OF CHARACTERS ******************
